4bcdfc3b85cf1d0b0accff84a5e616d238ad229d
yeimecrodriguez <70025865+yeimecrodriguez@users.noreply.github.com>
2021-04-30 01:59:47 -0500

    Add files via upload
    ejercicio auditoria 15  min

---
+++ AuditoriaDaniela_.java
@@ -0,0 +1,17 @@
+package entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-04-29T19:52:59")
+@StaticMetamodel(AuditoriaDaniela.class)
+public class AuditoriaDaniela_ { 
+
+    public static volatile SingularAttribute<AuditoriaDaniela, String> accion;
+    public static volatile SingularAttribute<AuditoriaDaniela, String> fecha;
+    public static volatile SingularAttribute<AuditoriaDaniela, String> contenido;
+    public static volatile SingularAttribute<AuditoriaDaniela, String> usuario;
+    public static volatile SingularAttribute<AuditoriaDaniela, Integer> id;
+
+}
